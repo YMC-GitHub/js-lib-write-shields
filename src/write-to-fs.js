88b292ed5e77a9c32f9328aa74712a3183f19090
yemiancheng <ymc.github@gmail.com>
2020-01-31 17:26:28 +0800

    build:update reslut to result

--- src/write-to-fs.js
+++ src/write-to-fs.js
@@ -1,9 +1,9 @@
 // he.config(key,val).writeShields().writeToFs()
 const writeToFs = function(opts) {
   let { utils, fs, PATH_TO_SAVE, FILE_TO_SAVE } = this.options;
-  let { reslut } = this.data;
+  let { result } = this.data;
 
   utils.mkdirsSync(PATH_TO_SAVE);
-  fs.writeFileSync(FILE_TO_SAVE, reslut, 'utf-8');
+  fs.writeFileSync(FILE_TO_SAVE, result, 'utf-8');
 };
 export default writeToFs;
